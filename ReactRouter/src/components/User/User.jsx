fbc79c25638c139f84a59485a9853a3c8b18dd23
OmBilthere <ombilthere953@gmail.com>
2025-09-27 06:13:36 +0000

    7 Hours Completed

---
+++ ReactRouter/src/components/User/User.jsx
@@ -0,0 +1,11 @@
+import {useParams} from 'react-router-dom'
+export default function() {
+ const {userid} = useParams()
+  return (
+    <div className='bg-gray-700 text-white text-3xl p-4 '>
+      User : {userid}
+    </div>
+  )
+}
+
+ 
--- ReactRouter/src/main.jsx
+++ ReactRouter/src/main.jsx
@@ -1,12 +1,60 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-const router = createBrowserRouter({
-  path:'/'
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import User from './components/User/User.jsx'
+// First way
+// const router = createBrowserRouter([
+ 
+//  {
+//    path:'/',
+//    element: <Layout /> ,
+   
+//    children: [
+//     {
+//       path:"",
+//       element: <Home />
+
+//     }, 
+//     {
+//       path: "about" , 
+//       element: <About />
+
+//     } ,
+
+//     {
+//       path: "contact" , 
+//       element: <Contact />
+
+//     }
+
+//    ]
+//  } 
   
-})
+// ])
+
+// 2nd way
+
+const router = createBrowserRouter (createRoutesFromElements(
+  <Route path='/' element ={<Layout />}>
+
+   <Route path= 'about' element={<About />} />
+   <Route path= 'contact' element={<Contact />} />
+   <Route path= 'home' element={<Home />} />
+   <Route path= 'user/:userid' element={<User />} />
+  {/* upar wala user component ka jo userid hum show krwana chahte h wo 
+  user file m bhi same name se honga chahiye ye hum variable input type ka router banane ke liye seekh rhe h */}
+
+
+  </Route>
+))
+
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
 
